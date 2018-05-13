518424041a33d6af80d5c51746e7678bbee9d799
Jse <josearias@upeu.edu.pe>
2018-05-12 21:21:10 -0500

    Initial commit

---
+++ src/main/java/hello/HelloWorldApplication.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Jse on 12/05/2018.
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+}
